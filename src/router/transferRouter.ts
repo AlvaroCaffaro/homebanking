1749fe2f121207b4fcef835073c1c1caad8986bc
alvaro caffaro <alvarocaffaro2004@gmail.com>
2025-02-07 03:46:40 -0300

    showing errors in tests

--- src/router/transferRouter.ts
+++ src/router/transferRouter.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { TransferController } from "../controller/transfer";
 
 export const transferRouter = Router();
-transferRouter.get('/agenda',TransferController.get_PersonsAgenda);
-transferRouter.post('/person',TransferController.get_personAccounts);
+transferRouter.get('/agenda',TransferController.get_PersonsAgenda); // quitar duplicados (cuando una persona estuvo).
+transferRouter.post('/person',TransferController.get_personAccounts); // verificar error (la funcion devuelve error)
 transferRouter.post('/',TransferController.find_account)
 transferRouter.post('/create',TransferController.create);
